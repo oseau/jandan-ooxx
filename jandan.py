fa688da4c323d24cab741271291fbde960347bd2
kai.yang <kai.yang@bugua.com>
2015-03-25 12:32:11 +0800

    first commit

---
+++ jandan.py
@@ -0,0 +1,23 @@
+__author__ = 'yang'
+
+from lxml import html
+import requests
+import os
+import time
+
+if not os.path.exists('imgs'):
+    os.makedirs('imgs')
+next_page = requests.get('http://jandan.net/ooxx')
+i=1
+while True:
+    tree = html.fromstring(next_page.text)
+    imgs = tree.xpath('//p//img[@*]')
+    for img in imgs:
+	url = img.attrib.values()[0] if len(img.attrib.values())==1 else img.attrib.values()[1]
+        if not os.path.exists('imgs/'+url.split('/')[-1]):
+            r = requests.get(url)
+            with open('imgs/'+url.split('/')[-1], 'w') as f:
+                f.write(r.content)
+    next_page = requests.get(tree.xpath("//a[@class='previous-comment-page']")[0].attrib['href'])
+    print('page %s downloaded' % i)
+    i=i+1
